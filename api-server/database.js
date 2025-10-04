a94d70750760c1c8ac16d5db73746a57d0460e5f
soyama <Soyamapango15@gmail.com>
2025-10-04 19:27:07 +0200

    Update sqlite3 to 5.1.7, fix database encryption setup

--- api-server/database.js
+++ api-server/database.js
@@ -1,11 +1,29 @@
 const knex = require('knex');
+const path = require('path');
 
+const dbPath = path.resolve(__dirname, 'database.db');
+
+// Knex configuration
 const db = knex({
   client: 'sqlite3',
   connection: {
-    filename: './database.db',
+    filename: dbPath,
+    
   },
   useNullAsDefault: true,
+  pool: {
+    afterCreate: (conn, done) => {
+      // Set encryption key for SQLCipher
+      conn.run("PRAGMA key = 'c9f8a2e7b1d4g6h3j0k2l5m8n7p1q4r3';", (err) => {
+        if (err) {
+          console.error('Error setting encryption key:', err.message);
+        } else {
+          console.log('Database encryption key set.');
+        }
+        done(err, conn);
+      });
+    },
+  },
 });
 
 module.exports = db;
